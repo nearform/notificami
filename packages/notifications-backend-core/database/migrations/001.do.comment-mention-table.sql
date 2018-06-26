b60d4596a4b6e86157b2e52e262569c4f4f469b3
Davide Fiorello <davide@codeflyer.com>
2018-06-26 16:10:08 +0200

    feat(core): added core db

--- packages/notifications-backend-core/database/migrations/001.do.comment-mention-table.sql
+++ packages/notifications-backend-core/database/migrations/001.do.comment-mention-table.sql
@@ -2,11 +2,18 @@ CREATE TABLE notification (
   id SERIAL PRIMARY KEY,
   notify JSON NOT NULL,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  read_at timestamp with time zone,
-  deleted_at timestamp with time zone,
-  last_notified_at timestamp with time zone,
+  read_at timestamp with time zone default null,
+  deleted_at timestamp with time zone default null,
   send_strategy varchar(256) default 'default',
-  notified_with JSON NOT null,
   user_identifier varchar(256) NOT null
 );
+
+CREATE TABLE sent_by (
+  id SERIAL PRIMARY KEY,
+  notification_id INTEGER NOT NULL REFERENCES notification (id) ON DELETE CASCADE,
+  channel varchar(255) NOT NULL,
+  sent_at timestamp with time zone NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS sent_by_notification_id_index ON sent_by (notification_id);
 
